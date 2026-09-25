b0267cf1cae6c6c1d7c28cccd520e4d8ebacd00d
Cabdinasira64 <cabdinasira64@gmail.com>
2026-09-25 08:21:25 +0300

    Waa assigment1 qeybtiisa 5

---
+++ Assigment1/5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Assigment5</title>
+</head>
+
+<body>
+    <?php
+
+    for ($i = 5; $i >= 1; $i--) {
+        echo "$i";
+    }
+
+    ?>
+
+</body>
+
+</html>
