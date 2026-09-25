bec45997f188759ebc5d732bf5ec53ab2ddf7cd2
Cabdinasira64 <cabdinasira64@gmail.com>
2026-09-25 08:30:38 +0300

    Waa assigment1 qeybtiisa 8

---
+++ Assigment1/8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Multiplication Table 12</title>
+</head>
+
+<body>
+    <?php
+
+
+    for ($i = 1; $i <= 12; $i++)
+        for ($j = 1; $j <= 12; $j++)
+            echo "Multiplcation $i * $j is = " . ($i * $j) . "<br>";
+
+
+    ?>
+
+
+</body>
+
+</html>
