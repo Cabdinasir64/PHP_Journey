fcfe3de94e5a77446c6542ecc8c8b00ec106f091
Cabdinasira64 <cabdinasira64@gmail.com>
2026-09-25 07:59:48 +0300

    NestedLoop

---
+++ Week2/NestedLoop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Nested Loop</title>
+</head>
+
+<body>
+    <?php
+
+
+    for ($i = 1; $i <= 3; $i++)
+        for ($j = 1; $j <= 5; $j++)
+            echo ("row $i and column $j Result is = " . ($i * $j)) . "<br>"
+
+    ?>
+
+</body>
+
+</html>
