4910c870c8b5a5747ced8c793bff225213140509
Cabdinasira64 <cabdinasira64@gmail.com>
2026-09-25 08:03:51 +0300

    Updated

--- Week2/NestedLoop.php
+++ Week2/NestedLoop.php
@@ -9,7 +9,7 @@
     <?php
 
 
-    for ($i = 1; $i <= 3; $i++)
+    for ($i = 1; $i <= 5; $i++)
         for ($j = 1; $j <= 5; $j++)
             echo ("row $i and column $j Result is = " . ($i * $j)) . "<br>"
 
